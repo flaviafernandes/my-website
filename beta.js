71072cea92b0f86a4627451fa7e3e527958ec137
Flavia Fernandes <aflaviamfernandes@gmail.com>
2019-12-24 19:05:02 -0300

    Add color box changing on mouse movement

--- beta.js
+++ beta.js
@@ -5,6 +5,8 @@ let my = document.getElementById("mouseY");
 console.log(mx, my);
 
 
+let colorBox = document.getElementById("color-box");
+let body = document.body;
 
 document.onmousemove = function handleMouseMove(event) { 
     let clientX = event.clientX;
@@ -13,4 +15,6 @@ document.onmousemove = function handleMouseMove(event) {
     mx.innerHTML = `mouse x : ${clientX}`;
     my.innerHTML = `mouse y : ${clientY}`;
 
+    colorBox.style.backgroundColor = `rgba(${clientX % 255}, ${clientY % 255}, ${3 * (clientX + clientY) % 255}, 0.3)`;
+    body.style.backgroundColor = `rgba(${clientY % 255}, ${clientX % 255}, ${(clientX + clientY + 100) % 255}, 0.3)`;
 }
