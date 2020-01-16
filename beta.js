351b7d96952128f99ce6ef4153ba80e79e481fc3
flaviafernandes <aflaviamfernandes@gmail.com>
2020-01-16 17:06:22 -0200

    design

--- beta.js
+++ beta.js
@@ -1,19 +1,22 @@
 console.log("oi gente");
 
-let mx = document.getElementById("mouseX");
-let my = document.getElementById("mouseY");
+var mx = document.getElementById("mouseX");
+var my = document.getElementById("mouseY");
 console.log(mx, my);
 
 
-let colorBox = document.getElementById("color-box");
-let body = document.body;
+var colorBox = document.getElementById("color-box");
+var body = document.body;
 
 document.onmousemove = function handleMouseMove(event) { 
-    let clientX = event.clientX;
-    let clientY = event.clientY;
+    var clientX = event.clientX;
+    var clientY = event.clientY;
 
-    mx.innerHTML = `mouse x : ${clientX}`;
-    my.innerHTML = `mouse y : ${clientY}`;
+    mx.innerHTML = "mouse x : " + clientX;
+    my.innerHTML = "mouse y : " + clientY;
+
+    var cx = clientX % 255;
+    var cy = clientY % 255;
 
     colorBox.style.backgroundColor = `rgba(${clientX % 255}, ${clientY % 255}, ${3 * (clientX + clientY) % 255}, 0.3)`;
     body.style.backgroundColor = `rgba(${clientY % 255}, ${clientX % 255}, ${(clientX + clientY + 100) % 255}, 0.3)`;
